79e715e1e8ec517b818641b93202e0c10775de61
Ryder Lewis <ryder.lewis@gmail.com>
2024-11-29 18:21:41 -0500

    day 2

--- days/day02.py
+++ days/day02.py
@@ -6,7 +6,24 @@ class Day02(Day):
         super().__init__(*args, **kwargs)
 
     def part1(self) -> str:
-        return "dayXX 1"
+        s = 0
+        for vals in self.parse():
+            v = sorted(vals)
+            s += v[-1] - v[0]
+        return str(s)
 
     def part2(self) -> str:
-        return "dayXX 2"
+        s = 0
+        for vals in self.parse():
+            v = sorted(vals)
+            for i in range(len(v)):
+                for j in range(i+1, len(v)):
+                    if v[j] % v[i] == 0:
+                        s += v[j]//v[i]
+        return str(s)
+
+    def parse(self) -> list[list[int]]:
+        return [
+            list(map(int, line.split()))
+            for line in self.data_lines()
+        ]
