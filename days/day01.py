424596d9d50c10603604fca3b050edb47ae9a740
Ryder Lewis <ryder.lewis@gmail.com>
2024-11-29 18:18:02 -0500

    day 1

--- days/day01.py
+++ days/day01.py
@@ -6,7 +6,11 @@ class Day01(Day):
         super().__init__(*args, **kwargs)
 
     def part1(self) -> str:
-        return "dayXX 1"
+        line = self.data_lines()[0]
+        return str(sum(int(c) for i, c in enumerate(line)
+                   if c == line[(i+1)%len(line)]))
 
     def part2(self) -> str:
-        return "dayXX 2"
+        line = self.data_lines()[0]
+        return str(sum(int(c) for i, c in enumerate(line)
+                       if c == line[(i+len(line)//2)%len(line)]))
