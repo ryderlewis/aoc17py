3239cef442b2881628570b7c6315b15670ede3d5
Ryder Lewis <ryder.lewis@gmail.com>
2024-11-30 02:17:14 -0500

    day 6

--- days/day06.py
+++ days/day06.py
@@ -6,7 +6,48 @@ class Day06(Day):
         super().__init__(*args, **kwargs)
 
     def part1(self) -> str:
-        return "dayXX 1"
+        banks = list(map(int, self.data_lines()[0].split()))
+        seen = {tuple(banks)}
+        while True:
+            v = max(banks)
+            i = banks.index(v)
+            next_banks = banks.copy()
+            next_banks[i] = 0
+            dist = v//(len(banks)-1)
+            for j in range(len(banks)):
+                if j != i:
+                    next_banks[j] += dist
+            remain = v % (len(banks)-1)
+            for j in range(1, remain+1):
+                next_banks[(i+j) % len(banks)] += 1
+            banks, t = next_banks, tuple(next_banks)
+
+            if t in seen:
+                return str(len(seen))
+            seen.add(t)
 
     def part2(self) -> str:
-        return "dayXX 2"
+        banks = list(map(int, self.data_lines()[0].split()))
+        seen = {tuple(banks)}
+        inf = False
+        while True:
+            v = max(banks)
+            i = banks.index(v)
+            next_banks = banks.copy()
+            next_banks[i] = 0
+            dist = v//(len(banks)-1)
+            for j in range(len(banks)):
+                if j != i:
+                    next_banks[j] += dist
+            remain = v % (len(banks)-1)
+            for j in range(1, remain+1):
+                next_banks[(i+j) % len(banks)] += 1
+            banks, t = next_banks, tuple(next_banks)
+
+            if t in seen:
+                if inf:
+                    return str(len(seen))
+                else:
+                    inf = True
+                    seen = set()
+            seen.add(t)
