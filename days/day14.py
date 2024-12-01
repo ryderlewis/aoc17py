f78b45b46baf62fec92fcae9fb91d2c9854f3bb3
Ryder Lewis <ryder.lewis@gmail.com>
2024-12-01 13:47:21 -0800

    day 14 part 2

--- days/day14.py
+++ days/day14.py
@@ -14,4 +14,43 @@ class Day14(Day):
     def part2(self) -> str:
         in_val = self.data_lines()[0]
         hashes = [dense_hash(f"{in_val}-{i}") for i in range(128)]
-        return "dayXX 2"
+        set_bits = set()
+        for row, h in enumerate(hashes):
+            for col, b in enumerate(self.bits(h)):
+                if b:
+                    set_bits.add((row, col))
+        group_count = 0
+        while set_bits:
+            group_count += 1
+            group = self.in_group(set_bits.pop(), set_bits)
+            set_bits -= group
+        return str(group_count)
+
+    @staticmethod
+    def bits(h: str) -> list[int]:
+        ret = []
+        for c in h:
+            v = int(c, 16)
+            ret.append((v >> 3) & 1)
+            ret.append((v >> 2) & 1)
+            ret.append((v >> 1) & 1)
+            ret.append((v >> 0) & 1)
+        return ret
+
+    @staticmethod
+    def in_group(val: tuple[int, int], all_vals: set[tuple[int, int]]) -> set[tuple[int, int]]:
+        seen = {val}
+        work = [val]
+        while work:
+            pos = work.pop()
+            neighbors = [
+                (pos[0]+1, pos[1]),
+                (pos[0]-1, pos[1]),
+                (pos[0], pos[1]-1),
+                (pos[0], pos[1]+1),
+            ]
+            for neighbor in neighbors:
+                if neighbor in all_vals and neighbor not in seen:
+                    seen.add(neighbor)
+                    work.append(neighbor)
+        return seen
