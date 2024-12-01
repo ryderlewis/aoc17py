095b85c834e9b39dca6e1da3b22277bf19c35897
Ryder Lewis <ryder.lewis@gmail.com>
2024-12-01 07:27:02 -0800

    day 10 part 2

--- days/day10.py
+++ days/day10.py
@@ -1,4 +1,5 @@
 from .day import Day
+from functools import reduce
 
 
 class Day10(Day):
@@ -26,4 +27,29 @@ class Day10(Day):
         return str(numbers[0]*numbers[1])
 
     def part2(self) -> str:
-        return "dayXX 2"
+        numbers = list(range(256))
+        curr_pos, skip_size = 0, 0
+        lengths = list(map(ord, self.data_lines()[0]))
+        lengths.extend([17, 31, 73, 47, 23])
+        for _ in range(64):
+            for length in lengths:
+                if length > 1:
+                    start_pos, end_pos = curr_pos, (curr_pos + length - 1) % len(numbers)
+                    if end_pos > start_pos:
+                        rev = list(reversed(numbers[start_pos:end_pos+1]))
+                        numbers[start_pos:end_pos+1] = rev
+                    elif end_pos < start_pos:
+                        rev = list(reversed(numbers[start_pos:] + numbers[:end_pos+1]))
+                        lpart, rpart = rev[:len(numbers[start_pos:])], rev[len(numbers[start_pos:]):]
+                        numbers[start_pos:] = lpart
+                        numbers[:end_pos+1] = rpart
+                curr_pos += length + skip_size
+                curr_pos %= len(numbers)
+                skip_size += 1
+
+        dense_hash = []
+        for i in range(0, len(numbers), 16):
+            nums = numbers[i:i+16]
+            digit = reduce(lambda x, y: x^y, nums, 0)
+            dense_hash.append(f"{digit:02x}")
+        return ''.join(dense_hash)
