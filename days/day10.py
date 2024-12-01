f7413f99a340a96944f65f9de1e87a3af7080918
Ryder Lewis <ryder.lewis@gmail.com>
2024-12-01 07:16:51 -0800

    day 10 part 1

--- days/day10.py
+++ days/day10.py
@@ -6,7 +6,24 @@ class Day10(Day):
         super().__init__(*args, **kwargs)
 
     def part1(self) -> str:
-        return "dayXX 1"
+        numbers = list(range(256))
+        curr_pos, skip_size = 0, 0
+        lengths = list(map(int, self.data_lines()[0].split(',')))
+        for length in lengths:
+            if length > 1:
+                start_pos, end_pos = curr_pos, (curr_pos + length - 1) % len(numbers)
+                if end_pos > start_pos:
+                    rev = list(reversed(numbers[start_pos:end_pos+1]))
+                    numbers[start_pos:end_pos+1] = rev
+                elif end_pos < start_pos:
+                    rev = list(reversed(numbers[start_pos:] + numbers[:end_pos+1]))
+                    lpart, rpart = rev[:len(numbers[start_pos:])], rev[len(numbers[start_pos:]):]
+                    numbers[start_pos:] = lpart
+                    numbers[:end_pos+1] = rpart
+            curr_pos += length + skip_size
+            curr_pos %= len(numbers)
+            skip_size += 1
+        return str(numbers[0]*numbers[1])
 
     def part2(self) -> str:
         return "dayXX 2"
