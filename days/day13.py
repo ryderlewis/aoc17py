1ab6d76188a736e4353cca5fefc9cafebc9488ee
Ryder Lewis <ryder.lewis@gmail.com>
2024-12-01 09:08:42 -0800

    day 13

--- days/day13.py
+++ days/day13.py
@@ -4,9 +4,37 @@ from .day import Day
 class Day13(Day):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.scanners = self.parse()
+
+    def severity(self, start_time: int) -> int:
+        severity = 0
+        scanners = self.scanners
+        for pico in range(max(scanners.keys())+1):
+            if pico in scanners:
+                depth = scanners[pico]
+                caught = (pico+start_time) % ((depth-1)*2) == 0
+                if caught:
+                    severity += pico * depth
+        return severity
+
+    def any_caught(self, start_time: int) -> bool:
+        return any((pico+start_time) % ((depth-1)*2) == 0
+                   for pico, depth in self.scanners.items())
 
     def part1(self) -> str:
-        return "dayXX 1"
+        return str(self.severity(0))
 
     def part2(self) -> str:
-        return "dayXX 2"
+        i = 0
+        while True:
+            if self.any_caught(i):
+                i += 1
+            else:
+                return str(i)
+
+    def parse(self) -> dict[int, int]:
+        scanners: dict[int, int] = {}
+        for line in self.data_lines():
+            pos, depth = map(int, line.split(': '))
+            scanners[pos] = depth
+        return scanners
