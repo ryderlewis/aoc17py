bad3f6c74b1b0a0d83104851f0d994e6130f89f0
Ryder Lewis <ryder.lewis@gmail.com>
2024-12-01 00:12:49 -0800

    day 9

--- days/day09.py
+++ days/day09.py
@@ -6,7 +6,54 @@ class Day09(Day):
         super().__init__(*args, **kwargs)
 
     def part1(self) -> str:
-        return "dayXX 1"
+        line = self.data_lines()[0]
+        score = 0
+        nest = 0
+        garbage = False
+
+        i = 0
+        while i < len(line):
+            c = line[i]
+            i += 1
+            if c == '!':
+                i += 1
+            elif garbage:
+                if c == '>':
+                    garbage = False
+            elif c == '<':
+                garbage = True
+            elif c == '{':
+                # new group starting
+                nest += 1
+                score += nest
+            elif c == '}':
+                nest -= 1
+            else:
+                # character within a group, but not garbage
+                pass
+
+        return str(score)
 
     def part2(self) -> str:
-        return "dayXX 2"
+        line = self.data_lines()[0]
+        score = 0
+        garbage = False
+
+        i = 0
+        while i < len(line):
+            c = line[i]
+            i += 1
+            if c == '!':
+                i += 1
+            elif garbage:
+                if c == '>':
+                    garbage = False
+                else:
+                    score += 1
+            elif c == '<':
+                garbage = True
+            else:
+                # character within a group, but not garbage
+                pass
+
+        return str(score)
